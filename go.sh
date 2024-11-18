0861877c86532a35b85dc2ba7241bb678fefcc4f
Curtis Rueden <ctrueden@wisc.edu>
2024-11-18 13:53:37 -0600

    Fix shell script

--- go.sh
+++ go.sh
@@ -6,7 +6,7 @@ test -d target/dependency || mvn dependency:copy-dependencies
 
 mvn package &&
   java \
-    -javaagent:target/headless-agent-1.0.0-SNAPSHOT.jar \
+    -javaagent:target/headless-awt-1.0.0-SNAPSHOT.jar \
     -Djava.awt.headless=true \
-    -cp target/headless-agent-1.0.0-SNAPSHOT.jar:target/headless-agent-1.0.0-SNAPSHOT-tests.jar:target/dependency/'*' \
+    -cp target/headless-awt-1.0.0-SNAPSHOT.jar:target/headless-awt-1.0.0-SNAPSHOT-tests.jar:target/dependency/'*' \
     org.scijava.headless.DoWindowThings
